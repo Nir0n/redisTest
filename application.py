2fa6bc0525af3b5caf5b690cd47e195e45a82a14
man-with-a-plan <vitaliy961@gmail.com>
2018-11-06 01:44:47 +0300

    log added and fixed get method

--- application.py
+++ application.py
@@ -1,10 +1,13 @@
 from connection import r
 import string
 import random
+import logging
 
 class Application(object):
+
     def __init__(self):
         self.conn = r
+        self.set_app_logger()
 
     def searchForRole(self):
         clientsList=r.client_list()
@@ -19,22 +22,35 @@ class Application(object):
 
     def executeTask(self):
         if self.conn.client_getname() == 'handler':
+            logging.info("Get sequence start")
             self.getMessage()
+            logging.info("Get sequence complete")
         else:
+            logging.info("Post sequence start")
             self.postMessage()
+            logging.info("Post sequence complete")
 
     def messageGenerator(self,size=10, chars=string.ascii_uppercase + string.digits):
-        return ''.join(random.choice(chars) for _ in range(size))
+        message=''.join(random.choice(chars) for _ in range(size))
+        logging.info("Post message: " + message)
+        return message
 
     def postMessage(self):
         return self.conn.rpush("messages",self.messageGenerator())
 
     def getMessage(self):
-        message = self.conn.rpop("messages")
+        message = self.conn.lpop("messages")
+        logging.info("Get message: " + message.decode("utf-8"))
         if (random.uniform(0, 100) > 95):
             self.setError(message)
+            logging.info("Error set" + message.decode("utf-8"))
 
     def setError(self, message):
         return self.conn.rpush("errors", message)
+
+    def set_app_logger(self):
+        logger=logging.basicConfig(filename='log.txt', format='%(asctime)s %(message)s',
+                                        level=logging.INFO)
+        return logger
 
 
